16c4c5e7881e13ba1f6b8503f5569ee5973d5427
raviswami2504 <137067725+raviswami2504@users.noreply.github.com>
2025-09-14 01:12:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/PassOp/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
 })
